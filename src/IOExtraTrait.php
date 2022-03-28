f53699a351f87e2a7f63296268183ff52211457e
Travis Tomka <underoathosx@gmail.com>
2022-03-27 19:46:16 -0600

    Add require question decorator to IOExtra.

--- src/IOExtraTrait.php
+++ src/IOExtraTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Pr0jectX\Px;
 
 use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 
 /**
  * Define the IO extra trait.
@@ -132,5 +133,38 @@ trait IOExtraTrait
         }
 
         return $this->formatQuestion("{$formattedQuestion}:");
+    }
+
+    /**
+     * Decorate a question as required.
+     *
+     * @param \Symfony\Component\Console\Question\Question $question
+     *   Input the question object.
+     * @param string $message
+     *   Input the message to use for the exception.
+     * @param callable|null $normalizeCallback
+     *   The question normalize callback.
+     *
+     * @return \Symfony\Component\Console\Question\Question
+     */
+    protected function requiredQuestion(
+        Question $question,
+        string $message = 'This field is required!',
+        ?callable $normalizeCallback = null
+    ): Question {
+        $question = ($question)->setValidator(function ($value) use ($message) {
+            if (!isset($value)) {
+                throw new \RuntimeException(
+                    $message
+                );
+            }
+            return $value;
+        });
+
+        if (is_callable($normalizeCallback)) {
+            $question->setNormalizer($normalizeCallback);
+        }
+
+        return $question;
     }
 }
