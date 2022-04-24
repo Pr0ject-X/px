289f7cba30afc618ea7a796fedc1d9c4cf1c618c
Travis Tomka <underoathosx@gmail.com>
2022-04-24 14:32:22 -0600

    Add merge capability to the JSON datastore.

--- src/Datastore/JsonDatastore.php
+++ src/Datastore/JsonDatastore.php
@@ -10,13 +10,39 @@ namespace Pr0jectX\Px\Datastore;
 class JsonDatastore extends DatastoreFilesystem
 {
     /**
+     * The content merge flag.
+     *
+     * @var bool
+     */
+    protected $merge = false;
+
+    /**
+     * Set the transform input to be merged.
+     *
+     * @return self
+     */
+    public function merge(): self
+    {
+        $this->merge = true;
+
+        return $this;
+    }
+
+    /**
      * {@inheritDoc}
      */
     protected function transformInput($content): string
     {
+        if ($this->merge) {
+            $content = array_replace_recursive(
+                $this->read(),
+                $content
+            );
+        }
+
         return json_encode(
             $content,
-            JSON_PRETTY_PRINT
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
         );
     }
 
